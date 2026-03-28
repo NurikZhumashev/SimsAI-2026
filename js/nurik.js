a5257dc1eb579afba3a6320a8e1bbe2c981d225b
NurikZhumashev <Nurkeldyzhumashev@gmail.com>
2026-03-29 03:16:25 +0500

    Update nurik.js

--- js/nurik.js
+++ js/nurik.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://simsai-2026-production.up.railway.app";
+window.BACKEND_URL = "https://simsai-2026-production.up.railway.app";
 // Полное состояние Нурика
 let stars = 50;
 let energy = 100;
